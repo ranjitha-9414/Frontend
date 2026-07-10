dee2f589863eee504740cea62c3b64a7556151a0
ranjitha-9414 <cranjitha82@gmail.com>
2026-07-10 10:52:16 +0530

    updated

--- Javascript/typesoffunction.js
+++ Javascript/typesoffunction.js
@@ -30,3 +30,33 @@ greet3();
 (function() {
     console.log("Hello, This is IIFE Function");
 })();
+
+// 4 ways of declaring function in javascript for adding 3 numbers
+
+// 1. Function Declaration
+
+function add(a,b,c) {
+    let sum = a + b + c;
+    console.log("Sum of 3 numbers using Function Declaration: " + sum);
+}
+add(10,20,30);
+
+// 2. Function Expression
+let Addition = function (a,b,c) {
+    let sum = a + b + c;
+    console.log("Sum of 3 numbers using Function Expression: " + sum);
+};
+Addition(40,50,60);
+
+// 3. Arrow Function
+let add2 = (a,b,c) => {
+    let sum = a + b + c;
+    console.log(`Sum of 3 numbers using Arrow Function: ${sum}`);
+};
+add2(70,80,90);
+
+// 4. IIFE (Immediately Invoked Function Expression)
+(function(a,b,c) {
+    let sum = a + b + c;
+    console.log(`Sum of 3 numbers using IIFE Function: ${sum}`);
+})(100,200,300);
