bfbb3567844f882363cc2c6a086d386f781cd42e
ranjitha-9414 <cranjitha82@gmail.com>
2026-08-07 10:41:42 +0530

    updated

--- Javascript/functionOverLoading.js
+++ Javascript/functionOverLoading.js
@@ -22,6 +22,18 @@ add(10,20);
 //... i can collect/pass the parameters/arguments from 0 to n , it is called rest parameter. 
 // It is basically an 1D array [] where it can consist parameters/arguments from 0 to n. It is used to pass variable number of arguments to a function. It is called rest parameter. It is an array that contains all the arguments passed to the function.
 
+
+//numbers=[10] , num =10 sum =0, sum=0+10 
+//numbers=[10,20] , num =10 sum =0, sum=0+10 , num=20 sum=10, sum=10+20 
+//numbers=[10,20,50] , num =10 sum =0, sum=0+10 , num=20 sum=10, sum=10+20 , num=50 sum=30, sum=30+50
 function add(...numbers) {
-    
+    let sum = 0;
+    for(let num of numbers) {
+        sum += num;
+    }
+    return sum;
 }
+
+console.log(add(10));
+console.log(add(10,20));
+console.log(add(10,20,50));
